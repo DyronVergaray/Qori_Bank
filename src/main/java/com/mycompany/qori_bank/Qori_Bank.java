b86c7f163e327e70b2ce6bb528d7da058e8ff987
DyronVergaray <u24220169@utp.edu.pe>
2026-06-10 20:20:03 -0500

    Se agrega el proyecto

---
+++ src/main/java/com/mycompany/qori_bank/Qori_Bank.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.qori_bank;
+
+
+public class Qori_Bank {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
