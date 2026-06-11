4890e454d58bc188abf475e6a101c5bf4df5c5e8
DyronVergaray <u24220169@utp.edu.pe>
2026-06-10 20:26:41 -0500

    Primer Push (Prueba)

--- src/main/java/com/mycompany/qori_bank/Qori_Bank.java
+++ src/main/java/com/mycompany/qori_bank/Qori_Bank.java
@@ -7,6 +7,7 @@ package com.mycompany.qori_bank;
 
 public class Qori_Bank {
 
+    // Primer comentario (Prueba)
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
